40ed88f98f27225af838a252b8062e866d3927ff
barankibar <barankibarr@outlook.com>
2023-08-03 20:37:01 +0300

    authorization services integration

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,21 @@
-package cmd
+package main
+
+import (
+	"log"
+
+	"github.com/barankibar/shopicity-svc-gateway/pkg/auth"
+	"github.com/barankibar/shopicity-svc-gateway/pkg/config"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	c, err := config.LoadConfig()
+	if err != nil {
+		log.Fatalln("Failed at config: ", err)
+	}
+	r := gin.Default()
+
+	auth.RegisterRoutes(r, &c)
+
+	r.Run(c.Port)
+}
